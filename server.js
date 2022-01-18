16d6b3efbad775ee93064faae80aca04de6d7862
Mickael55 <mickaelhodel@orange>
2022-01-18 11:01:33 +0100

    add naive implementation of backend node

---
+++ server.js
@@ -0,0 +1,49 @@
+//creer un serveur Node dans le fichier server.js
+const http = require('http');//require permet d'importer le contenu d'un module Javascript
+const app = require('./app');
+
+//Ajout de a normalisation de port, la gestion d'erreur et du logging basique à votre serveur Node le rend plus constant et plus facile à déboguer
+const normalizePort = val => {// renvoie un port valide, qu'il soit fourni sous la forme d'un numéro ou d'une chaîne
+    const port = parseInt(val, 10);
+  
+    if (isNaN(port)) {
+      return val;
+    }
+    if (port >= 0) {
+      return port;
+    }
+    return false;
+  };
+  const port = normalizePort(process.env.PORT || '3000');
+  app.set('port', port);
+  
+  const errorHandler = error => {//recherche les différentes erreurs et les gère de manière appropriée. Elle est ensuite enregistrée dans le serveur
+    if (error.syscall !== 'listen') {
+      throw error;
+    }
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+    switch (error.code) {
+      case 'EACCES':
+        console.error(bind + ' requires elevated privileges.');
+        process.exit(1);
+        break;
+      case 'EADDRINUSE':
+        console.error(bind + ' is already in use.');
+        process.exit(1);
+        break;
+      default:
+        throw error;
+    }
+  };
+  
+  const server = http.createServer(app);
+  
+  server.on('error', errorHandler);
+  server.on('listening', () => {
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+    console.log('Listening on ' + bind);
+  });
+  
+  server.listen(port);
